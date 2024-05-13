3a697e871c9d52caac23af34620aa88c263eeb5c
Tyler Boyd <ty@tyboyd.dev>
2024-05-13 16:44:23 -0500

    added some basic file reading and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,12 @@ fn main() {
     let args = Cli::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file at path");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
